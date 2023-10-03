12f3125b3789d1eb0f176664fda18f95866e51a2
techbabette <57938200+techbabette@users.noreply.github.com>
2023-10-03 09:44:00 +0200

    Cart tests

---
+++ tests/cart.spec.js
@@ -0,0 +1,44 @@
+const {authenticatedTest, expect } = require('./setup.js');
+authenticatedTest("Add and remove items from cart.", async ({authenticatedPage}) =>{
+    await authenticatedPage.goto("/inventory.html");
+
+    let addedItemName;
+    let item;
+    let inventoryButton;
+
+    await authenticatedTest.step("Find first item and add to cart", async () => {
+        item = await authenticatedPage.locator(".inventory_item").first();
+        addedItemName = await item.locator(".inventory_item_name").textContent();
+
+        inventoryButton = await item.locator(".btn_inventory");
+        await inventoryButton.click();
+    })
+
+    await authenticatedTest.step("Check if cart number went up", async () => {
+        let cartNumber = await authenticatedPage.locator(".shopping_cart_badge").textContent();
+
+        expect(parseInt(cartNumber) === 1).toBeTruthy();
+    })
+
+    await authenticatedTest.step("Remove item from cart", async () => {
+        await inventoryButton.click();
+    })
+
+    await authenticatedTest.step("Check if cart number went down (badge hidden)", async () => {
+        await expect(authenticatedPage.locator(".shopping_cart_badge")).not.toBeVisible
+    })
+
+    await authenticatedTest.step("Readd item to cart", async() => {
+        await inventoryButton.click();
+    })
+
+    await authenticatedTest.step("Navigate to cart", async() => {
+        await authenticatedPage.locator(".shopping_cart_link").click();
+        await expect(authenticatedPage).toHaveURL("/cart.html");
+    })
+
+    await authenticatedTest.step("Check if added item in cart", async() => {
+        let nameOfCartItem = await authenticatedPage.locator(".inventory_item_name").first().textContent();
+        expect(nameOfCartItem === addedItemName).toBeTruthy();
+    })
+  })
